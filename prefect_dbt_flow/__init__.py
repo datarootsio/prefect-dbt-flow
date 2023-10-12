d7ec7943861d27241693927a9d013b773f805ab6
David <david@dataroots.io>
2023-10-12 11:17:38 +0200

    removed setup files, skiped ruff imports

--- prefect_dbt_flow/__init__.py
+++ prefect_dbt_flow/__init__.py
@@ -1,3 +1,8 @@
 """Prefect dbt flow initialization"""
-from prefect_dbt_flow.dbt import DbtDagOptions, DbtNode, DbtProfile, DbtProject
+from prefect_dbt_flow.dbt import (
+    DbtDagOptions,
+    DbtNode,
+    DbtProfile,
+    DbtProject,
+)  # ruff: skip
 from prefect_dbt_flow.flow import dbt_flow  # noqa: F401
